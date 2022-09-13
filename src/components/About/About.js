08251b9887b1fc3857d5cd67cc401bc4a893356e
jcochran206 <jonathan.o.cochran@gmail.com>
2022-09-12 22:43:11 -0700

    started about pac 12, needs design, and styling

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import pac12Data from "../../data/pac12.json";
+
+const About = () => {
+  return (
+    <div>
+        <h1>Pac 12 Conference</h1>
+        <h3>Where Champions Play</h3>
+    </div>
+  )
+}
+
+export default About
