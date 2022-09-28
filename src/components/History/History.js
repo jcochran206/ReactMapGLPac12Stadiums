e7a21250ea5874b139286dec3c3533c2ede638f5
jcochran206 <jonathan.o.cochran@gmail.com>
2022-09-27 22:39:29 -0700

    added component for history and details for build

---
+++ src/components/History/History.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const History = () => {
+  return (
+    <div>
+        History of the Pac 12 Champions go Here 
+        create an faq dropdown 
+        build tabs section for each teams
+    </div>
+  )
+}
+
+export default History
